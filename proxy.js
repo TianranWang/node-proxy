6a72475d9f83ec96b332d815efb8e8b9b513596c
XuYaguang <yaguang.xu@ele.me>
2016-10-13 20:22:32 +0800

    测试成功

--- proxy.js
+++ proxy.js
@@ -1,12 +1,12 @@
 const http = require('http')
 const querystring = require('querystring')
 /**
- * 原理：起一个服务，接受到请求解析，再转发给服务器
+ * author: sunopar
  */
 
-function proxy(http,host,port){
+function proxy(host,port=80,proxyPort=4040){
     let _http = require('http')
-    let data = ''
+    let data  = '';
     const server = http.createServer((req,res)=>{
         switch(req.method){
             case 'GET':
@@ -22,36 +22,46 @@ function proxy(http,host,port){
         }
         
         
-    }).listen(4040,()=>{
+    }).listen(proxyPort,()=>{
         console.log('proxy listen on port 4040')
     })
 
     function _proxyGet(req,res){
-        _http.get(`${host}${port}/${req.url}`,proxyRes=>{
+        _http.get(`${host}${req.url}`,proxyRes=>{
             pipeRes(proxyRes,res)       
         })
     }
     function pipeRes(proxyRes,res){
         res.writeHead(proxyRes.statusCode,proxyRes.headers);
-        proxyRes.pipe(res)
-        res.end()
+        proxyRes.pipe(res,{end:false})
+        proxyRes.on('end',()=>{
+            res.end()
+        })
     }
     function _proxyPost(req,res){
+
         let opt = {
             method:req.method,
-            host,
+            host:,
             port,
             path:req.url,
-            headers:req,headers
+            headers:req.headers,
         }
         req.addListener('data',chunk=>{
             data+=chunk
         })
-        .addListener('end'()=>{
-            data = querystring.parse(data)
-            _http.request(opt,proxyRes=>{
+        .addListener('end',()=>{
+            let request = _http.request(opt,proxyRes=>{
                 pipeRes(proxyRes,res)
             })
+            request.write(data);
+            request.end();
         })
     }
 }
+
+
+
+
+
+
